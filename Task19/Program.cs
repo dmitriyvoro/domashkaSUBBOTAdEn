69849228de17aff4a80e51c4d1eaeeeb0a61f2e6
Димка я <  @mail.ru>
2022-09-30 20:47:31 +0300

    Task 19 palindrom

---
+++ Task19/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("введите пятизначное число: ");
+string array = Console.ReadLine();
+if (array[0]==array[4] && array[1] == array [3])
+Console.WriteLine("yes");
+else Console.WriteLine("no");
