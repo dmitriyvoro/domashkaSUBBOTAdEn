7ea08164ec6918d6ab91908e594291781ea735d5
Димка я <  @mail.ru>
2022-09-30 20:52:37 +0300

    Task21 3D prostranstvo

---
+++ Task21/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите координату x1 : ");
+int x1= Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату x2 : ");
+int x2= Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z1 : ");
+int z1= Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double S = Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2));
+Console.WriteLine(Math.Round(S,2));
